90b0990d5419350d2721bc6173350c0558be5c72
J-Robles <145251089+Pollux1989@users.noreply.github.com>
2024-10-15 11:27:47 +1000

    creacion de conector a la base de datos Sin pruebas

---
+++ src/app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+import database as db
+
+template_dir = os.path.dirname(os.path.abspath(os.path.dirname(__file__)))
+template_dir = os.path.join(template_dir, 'src', 'templates')
+
+app = Flask(__name__, template_folder = template_dir)
+
+#Rutas de la aplicacion
+
+@app.route('/')
+
+def home():
+    return render_template('index.html')
+
+if __name__ =='__main__':
+    app.run(debug=True, port=4000)
+
---
+++ src/database.py
@@ -0,0 +1,8 @@
+import mysql.connector
+
+database = mysql.connector.connect(
+    host = 'labmedia.tech',
+    user = 'u978383340_gym_tracker',
+    password = 'Australia2024*',
+    database= 'u978383340_useraccounts',
+)
